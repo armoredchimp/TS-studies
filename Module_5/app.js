110644161afaf09c0ddcca060f5547c66ab5d080
armoredchimp <mattkasmir@gmail.com>
2023-10-18 20:46:21 -0700

    10/18

--- Module_5/app.js
+++ Module_5/app.js
@@ -1,9 +1,27 @@
 var Derpartment = /** @class */ (function () {
-    function Derpartment(n) {
-        this.name = "Default";
-        this.name = n;
+    function Derpartment(id, name) {
+        this.id = id;
+        this.name = name;
+        // private name: string = "Default";
+        this.employees = [];
+        // this.name = n;
     }
+    Derpartment.prototype.describe = function () {
+        console.log("Derpatment" + this.id + this.name);
+    };
+    Derpartment.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Derpartment.prototype.printEmployeeInfo = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Derpartment;
 }());
-var dept1 = new Derpartment("Dept1");
+var dept1 = new Derpartment("1", "Dept1");
+dept1.addEmployee("bill");
+dept1.addEmployee("sally");
+dept1.describe();
+var dept1Copy = { name: "s", describe: dept1.describe };
+// dept1Copy.describe();
 console.log(dept1);
--- Module_5/app.ts
+++ Module_5/app.ts
@@ -1,11 +1,31 @@
 class Derpartment {
-  name: string = "Default";
+  // private name: string = "Default";
+  private employees: string[] = [];
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
+  }
 
-  constructor(n: string) {
-    this.name = n;
+  describe(this: Derpartment) {
+    console.log("Derpatment" + this.id + this.name);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInfo() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const dept1 = new Derpartment("Dept1");
+const dept1 = new Derpartment("1", "Dept1");
 
+dept1.addEmployee("bill");
+dept1.addEmployee("sally");
+
+dept1.describe();
+
+const dept1Copy = { name: "s", describe: dept1.describe };
+// dept1Copy.describe();
 console.log(dept1);

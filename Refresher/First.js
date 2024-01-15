e12708a76497c72233cbdae80391fd1ecc06596a
armoredchimp <mattkasmir@gmail.com>
2024-01-15 10:09:36 -0800

    1/15

--- Refresher/First.js
+++ Refresher/First.js
@@ -1,2 +1,12 @@
 var a = 'hi';
+var arr = [];
+arr.push('str');
+arr.push(5);
 console.log(a);
+function generateEmail(input) {
+    return "".concat(input.firstName, ".").concat(input.lastName);
+}
+console.log(generateEmail({
+    firstName: 'John',
+    lastName: 'Doe'
+}));
--- Refresher/first.ts
+++ Refresher/first.ts
@@ -5,3 +5,21 @@ arr.push('str')
 arr.push(5 as any);
 
 console.log(a)
+
+type job = 'Engineer' | 'Garbage Man'
+
+interface Person {
+  firstName: string,
+  lastName: string,
+  job: job
+}
+
+function generateEmail(input: Person): string {
+  return `${input.firstName}.${input.lastName}`
+}
+
+console.log(generateEmail({
+  firstName: 'John',
+  lastName: 'Doe',
+  job: 'Garbage Man'
+}))
